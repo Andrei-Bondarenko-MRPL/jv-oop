225267b7fa4a05ca93d51cdea55179bdf61abde4
Andrei_Bondarenko <bondorol007@gmail.com>
2022-10-27 20:24:10 +0300

    Exercise OOP Basic java core/oop

---
+++ src/main/java/BuilderMachines/Excavator.java
@@ -0,0 +1,14 @@
+package BuilderMachines;
+
+public class Excavator extends Machine {
+
+    @Override
+    public void doWork() {
+        System.out.println("Excavator do work");
+    }
+
+    @Override
+    public void stopWork() {
+        System.out.println("Excavator stop work");
+    }
+}
---
+++ src/main/java/BuilderMachines/Machine.java
@@ -0,0 +1,6 @@
+package BuilderMachines;
+
+public abstract class Machine {
+    public abstract void doWork();
+    public abstract void stopWork();
+}
